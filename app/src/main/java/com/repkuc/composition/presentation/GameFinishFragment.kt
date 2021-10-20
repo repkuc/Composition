86980e4f2ed21ee69bcf9f98a57c72a0e6bd2e9c
repkuc <arturs.gerkis@inbox.lv>
2021-10-20 14:17:50 +0300

    lesson3 done Presentation

---
+++ app/src/main/java/com/repkuc/composition/presentation/GameFinishFragment.kt
@@ -0,0 +1,21 @@
+package com.repkuc.composition.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.repkuc.composition.R
+
+
+class GameFinishFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_game_finish, container, false)
+    }
+
+
+}
