8b0bcfed1ba920b00238b5164af384571c48eaa3
repkuc <arturs.gerkis@inbox.lv>
2021-10-20 14:43:21 +0300

    lesson4 done Added viewBinding

--- app/src/main/java/com/repkuc/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/repkuc/composition/presentation/GameFragment.kt
@@ -1,20 +1,30 @@
 package com.repkuc.composition.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.repkuc.composition.R
+import androidx.fragment.app.Fragment
+import com.repkuc.composition.databinding.FragmentGameBinding
 
 
 class GameFragment : Fragment() {
+
+    private var _binding: FragmentGameBinding? = null
+    private val binding: FragmentGameBinding
+        get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
